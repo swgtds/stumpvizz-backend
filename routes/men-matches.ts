03469be7bf37adb780dbbfe3659c401330fab750
swgtds <swagatadas.sd@outlook.com>
2025-03-01 19:43:20 +0530

    chore: update match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -4,72 +4,19 @@ const router = Router();
 
 const menMatches = [
   {
-    id: "ban-vs-nz",
+    id: "ind-vs-nz",
     name: "Live Match 1",
     startTime: "14:15",
-    endTime: "22:30",
+    endTime: "23:30",
     streamUrl: "/videos/jiostar.html",
     match: {
-      team1: "Bangldesh",
+      team1: "India",
       team2: "New Zealand",
-      date: "2025-02-24",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
-    },
-  },
-  {
-    id: "aus-vs-sa",
-    name: "Live Match 2",
-    startTime: "14:15",
-    endTime: "22:30",
-    streamUrl: "/videos/jiostar.html",
-    match: {
-      team1: "Australia",
-      team2: "South Africa",
-      date: "2025-02-25",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
-    },
-  },
-  {
-    id: "afg-vs-eng",
-    name: "Live Match 3",
-    startTime: "14:15",
-    endTime: "22:30",
-    streamUrl: "/videos/jiostar.html",
-    match: {
-      team1: "Afghanistan",
-      team2: "England",
-      date: "2025-02-26",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
-    },
-  },
-  {
-    id: "pak-vs-ban",
-    name: "Live Match 4",
-    startTime: "14:15",
-    endTime: "22:30",
-    streamUrl: "/videos/jiostar.html",
-    match: {
-      team1: "Pakistan",
-      team2: "Bangladesh",
-      date: "2025-02-27",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
-    },
-  },
-  {
-    id: "afg-vs-aus",
-    name: "Live Match 5",
-    startTime: "14:15",
-    endTime: "22:30",
-    streamUrl: "/videos/jiostar.html",
-    match: {
-      team1: "Afghanistan",
-      team2: "Australia",
-      date: "2025-02-28",
+      date: "2025-03-02",
       thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
     },
   },
 ];
-
 router.get("/", (req, res) => {
   res.json(menMatches);
 });
