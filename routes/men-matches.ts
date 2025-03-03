08ef22528fc4cd62bedb8a0af2591a6019e73e9e
swgtds <swagatadas.sd@outlook.com>
2025-03-03 17:23:39 +0530

    chore: update match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -12,7 +12,7 @@ const menMatches = [
     match: {
       team1: "India",
       team2: "Australia(SF-1)",
-      date: "2025-03-02",
+      date: "2025-03-04",
       thumbnail: "https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Findia-vs-australia-ct-2025-semi-final-v0-dxnpj0vtwame1.jpeg%3Fwidth%3D1080%26crop%3Dsmart%26auto%3Dwebp%26s%3D56e3bd960d439d518d933694fc9469329d78fc01",
     },
   },
@@ -25,7 +25,7 @@ const menMatches = [
     match: {
       team1: "South Africa",
       team2: "New Zealand(SF-2)",
-      date: "2025-03-02",
+      date: "2025-03-05",
       thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
     },
   },
