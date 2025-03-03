304e840b373812327572d7a0a920b11aa28a1119
swgtds <swagatadas.sd@outlook.com>
2025-03-03 17:22:57 +0530

    chore: update thumbnail

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -4,14 +4,27 @@ const router = Router();
 
 const menMatches = [
   {
-    id: "ind-vs-nz",
+    id: "ind-vs-aus",
     name: "Live Match 1",
     startTime: "14:15",
-    endTime: "23:30",
-    streamUrl: "/videos/prime.html",
+    endTime: "23:00",
+    streamUrl: "/videos/criclife.html",
     match: {
       team1: "India",
-      team2: "New Zealand",
+      team2: "Australia(SF-1)",
+      date: "2025-03-02",
+      thumbnail: "https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Findia-vs-australia-ct-2025-semi-final-v0-dxnpj0vtwame1.jpeg%3Fwidth%3D1080%26crop%3Dsmart%26auto%3Dwebp%26s%3D56e3bd960d439d518d933694fc9469329d78fc01",
+    },
+  },
+  {
+    id: "sa-vs-nz",
+    name: "Live Match 2",
+    startTime: "14:15",
+    endTime: "23:00",
+    streamUrl: "/videos/criclife.html",
+    match: {
+      team1: "South Africa",
+      team2: "New Zealand(SF-2)",
       date: "2025-03-02",
       thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
     },
