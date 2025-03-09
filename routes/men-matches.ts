57bae6c189e6ac8dd4dbc8bb855be6bf39bda61d
swgtds <swagatadas.sd@outlook.com>
2025-03-09 13:15:06 +0530

    chore: update match thumbnail and time

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -6,27 +6,14 @@ const menMatches = [
   {
     id: "ind-vs-sa",
     name: "Live Match 1",
-    startTime: "14:15",
+    startTime: "14:00",
     endTime: "23:00",
     streamUrl: "/videos/prime.html",
     match: {
       team1: "India",
       team2: "New Zealand(Final)",
       date: "2025-03-09",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
-    },
-  },
-  {
-    id: "sa-vs-nz",
-    name: "Live Match 2",
-    startTime: "14:15",
-    endTime: "23:00",
-    streamUrl: "/videos/prime.html",
-    match: {
-      team1: "South Africa",
-      team2: "New Zealand(SF-2)",
-      date: "2025-03-05",
-      thumbnail: "https://thesportsprince.com/wp-content/uploads/2024/12/7-2.webp",
+      thumbnail: "https://img.jagranjosh.com/images/2025/March/532025/ind-vs-nz-icc-champions-trophy-2025.webp",
     },
   },
 ];
