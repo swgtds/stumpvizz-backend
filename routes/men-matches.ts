88ea81b0b21b897eeef8aa098abbfb579bae046e
Anick Sarkar <anicksarkar18032003@gmail.com>
2025-03-23 10:41:25 +0530

    updated match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -16,6 +16,45 @@ const menMatches = [
       thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
     },
   },
+  {
+    id: "csk-vs-mi",
+    name: "Live Match 2",
+    startTime: "19:00",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Chennai Super Kings",
+      team2: "Mumbai Indians",
+      date: "2025-03-23",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "dc-vs-lsg",
+    name: "Live Match 3",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Delhi Capitals",
+      team2: "Lucknow Super Giants",
+      date: "2025-03-24",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "gt-vs-pbks",
+    name: "Live Match 4",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Gujarat Titans",
+      team2: "punjab Kings",
+      date: "2025-03-25",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
 ];
 router.get("/", (req, res) => {
   res.json(menMatches);
