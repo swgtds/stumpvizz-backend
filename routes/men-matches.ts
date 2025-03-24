13eef424239836ea8d2d05de8a070a5efc42f406
SarkarAnick <164182596+SarkarAnick@users.noreply.github.com>
2025-03-24 17:24:34 +0530

    updated match fixtures (#3)
    * update match fixtures
    
    * updated match fixtures
    
    * updated match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -3,16 +3,83 @@ import { Router } from "express";
 const router = Router();
 
 const menMatches = [
+ 
+  
   {
-    id: "srh-vs-rr",
-    name: "Live Match 1",
-    startTime: "14:00",
-    endTime: "18:30",
+    id: "dc-vs-lsg",
+    name: "Live Match 3",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Delhi Capitals",
+      team2: "Lucknow Super Giants",
+      date: "2025-03-24",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "gt-vs-pbks",
+    name: "Live Match 4",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Gujarat Titans",
+      team2: "punjab Kings",
+      date: "2025-03-25",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "rr-vs-kkr",
+    name: "Live Match 5",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Rajasthan Royals",
+      team2: "Kolkata Knight Riders",
+      date: "2025-03-26",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "srh-vs-lsg",
+    name: "Live Match 6",
+    startTime: "18:30",
+    endTime: "23:30",
     streamUrl: "/videos/t-sports.html",
     match: {
       team1: "Sunrisers Hyderabad",
-      team2: "Rajasthan Royals",
-      date: "2025-03-23",
+      team2: "Lucknow Super Giants",
+      date: "2025-03-27",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "csk-vs-rcb",
+    name: "Live Match 7",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Chennai Super Kings",
+      team2: "Royal Challengers Bangalore",
+      date: "2025-03-28",
+      thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
+    },
+  },
+  {
+    id: "gt-vs-mi",
+    name: "Live Match 8",
+    startTime: "18:30",
+    endTime: "23:30",
+    streamUrl: "/videos/t-sports.html",
+    match: {
+      team1: "Gujarat Titans",
+      team2: "Mumbai Indians",
+      date: "2025-03-29",
       thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
     },
   },
