02d8956c5c42381eec90658d751909f910afb6f3
swgtds <swagatadas.sd@outlook.com>
2025-04-02 18:11:38 +0530

    chore: update match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -6,13 +6,13 @@ const menMatches = [
   {
     id: "mi-vs-kkr",
     name: "Live Match 1",
-    startTime: "18:30",
+    startTime: "18:00",
     endTime: "23:30",
     streamUrl: "/videos/willow-cricket-1.html",
     match: {
-      team1: "Mumbai Indians",
-      team2: "Kolkata Knight Riders",
-      date: "2025-03-31",
+      team1: "Royal Challengers Bangalore",
+      team2: "Gujarat Titans",
+      date: "2025-04-02",
       thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
     },
   },
