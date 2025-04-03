c71e196ba818b6b5b3fa14df3361928e54a3e53b
swgtds <swagatadas.sd@outlook.com>
2025-04-03 20:57:01 +0530

    chore: update match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 
 const router = Router();
-const streamLink = "/videos/willow-cricket-1.html"; 
+const streamLink = "https://webplayer-live.pages.dev/?url=https://ee4ee8c03a26.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.755921704393.channel.LOohfipmCQDP.m3u8"; 
 
 const menMatches = [
   {
-    id: "rcb-vs-gt",
+    id: "kkr-vs-srh",
     name: "Live Match 1",
     startTime: "18:00",
     endTime: "23:30",
     streamUrl: streamLink,
     match: {
-      team1: "Royal Challengers Bangalore",
-      team2: "Gujarat Titans",
-      date: "2025-04-02",
+      team1: "Kolkata Knight Riders",
+      team2: "Sunrisers Hyderabad",
+      date: "2025-04-03",
       thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
     },
   },
