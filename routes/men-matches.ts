1efdc6b38d61ff433d47d67a6d3069d4d883903b
swgtds <swagatadas.sd@outlook.com>
2025-04-04 19:27:40 +0530

    chore: update match fixtures

--- routes/men-matches.ts
+++ routes/men-matches.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 
 const router = Router();
-const streamLink = "https://webplayer-live.pages.dev/?url=https://ee4ee8c03a26.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.755921704393.channel.LOohfipmCQDP.m3u8"; 
+const streamLink = "https://webplayer-live.pages.dev/?url=https://prod-ent-live-gm.jiocinema.com/bpk-tv/JS_M1_IPL25_ENG_IDC_MOB/Fallback/index.m3u8"; 
 
 const menMatches = [
   {
-    id: "kkr-vs-srh",
+    id: "lsg-vs-mi",
     name: "Live Match 1",
-    startTime: "18:00",
+    startTime: "19:00",
     endTime: "23:30",
     streamUrl: streamLink,
     match: {
-      team1: "Kolkata Knight Riders",
-      team2: "Sunrisers Hyderabad",
-      date: "2025-04-03",
+      team1: "Lucknow Super Giants",
+      team2: "Mumbai Indians",
+      date: "2025-04-04",
       thumbnail: "https://www.fantasyarena.in/images/1644927202_ipl.jpg",
     },
   },
